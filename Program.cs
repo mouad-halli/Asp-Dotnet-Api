4653b589c1d60f278cce2300aa77b15360f661d3
mouad halli <mouadhalli1337@gmail.com>
2024-08-03 23:47:52 +0100

    now authentication and authorization is done using JWT that it is stored inside an HTTP only cookie

--- Program.cs
+++ Program.cs
@@ -5,6 +5,9 @@ using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using Microsoft.AspNetCore.Identity;
 using FirstAPI.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using FirstAPI.interfaces;
+using FirstAPI.services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -30,20 +33,58 @@ builder.Services.AddIdentity<User, IdentityRole>()
 var jwtIssuer = builder.Configuration.GetSection("Jwt:Issuer").Get<string>() ?? throw new InvalidOperationException("jwt issuer not found");
 var jwtKey = builder.Configuration.GetSection("Jwt:Key").Get<string>() ?? throw new InvalidOperationException("jwt key not found");
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
-{
-     options.TokenValidationParameters = new TokenValidationParameters
-     {
-         ValidateIssuer = true,
-         ValidateAudience = true,
-         ValidateLifetime = true,
-         ValidateIssuerSigningKey = true,
-         ValidIssuer = jwtIssuer,
-         ValidAudience = jwtIssuer,
-         // using null-coalescing operators to protect passing potential null value
-         IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtKey ?? ""))
-     };
- });
+builder.Services.AddAuthentication(options => {
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+    {
+        options.Cookie.Name = "access_token";
+        options.Cookie.HttpOnly = true; // XSS protection by preventing browser javascript from accessing the cookie
+        // options.Cookie.SecurePolicy = CookieSecurePolicy.Always; // Always use HTTPS
+        options.Cookie.SameSite = SameSiteMode.Strict; // CSRF protection
+    })
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtIssuer,
+            ValidAudience = jwtIssuer,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+        };
+        options.Events = new JwtBearerEvents
+        {
+        /*
+            Since we are storing the JWT token in a cookie rather than as a Bearer token in the Authorization header
+            (where the JwtBearer middleware typically expects it), we need to implement custom extraction logic.
+            This logic will retrieve the token from the cookie in the HTTP request when a user accesses a route that requires authorization.
+        */
+            OnMessageReceived = context =>
+            {
+                // if cookie named access_token was found it is stored inside a variable named accessToken
+                context.Request.Cookies.TryGetValue("access_token", out var accessToken);
+
+                if (!string.IsNullOrEmpty(accessToken))
+                    // setting the token for authorization
+                    context.Token = accessToken;
+                
+                return Task.CompletedTask;
+            },
+            // OnAuthenticationFailed = context =>
+            // {
+            //     return Task.CompletedTask;
+            // }
+        };
+    }
+);
+
+// registering service for dependency injection with AddScoped method
+builder.Services.AddScoped<ITokenService, TokenService>();
+
 
 var app = builder.Build();
 
